fce0f69441dd39b28b87a18b2b3fc62e7bcd9c07
Franco <franco.mana@hotmail.com>
2021-11-18 12:34:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
+    }
+    /**
+     * ejecuta y muesta el valor
+     */
+    private fun rollDice() {
+        // crea un dado de 6 caras
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // busca la imagen by id
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // seguncual num imp la img
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // update img
+        diceImage.setImageResource(drawableResource)
+        // update desc
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
